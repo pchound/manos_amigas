bff6d56bc6df4c03361c38188e4aa0cc3a0a7729
pchound <82906754+pchound@users.noreply.github.com>
2021-07-21 11:16:00 -0600

    Add files via upload

---
+++ login5/src/module-info.java
@@ -0,0 +1,14 @@
+module login2 {
+
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires java.sql;
+    //requires mysql.connector.java;
+    //requires sqlite.jdbc;
+    //requires org.xerial.sqlitejdbc;
+
+
+
+    opens sample;
+}
